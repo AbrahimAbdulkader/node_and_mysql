a7498971ec61b9fbb2f6ad3c5571616c4cae87cc
Abrahim Abdulkader <abrahimabdulkader370@gmail.com>
2022-05-16 15:11:20 +0000

    Faker Function

--- app.js
+++ app.js
@@ -1,2 +1,13 @@
 const { faker } = require('@faker-js/faker');
-console.log(faker.internet.email()); 
+console.log(faker.internet.email()); //Prints a random email
+console.log(faker.internet.past()); //Prints a random past date 
+console.log(faker.address.city()); //Prints a random city 
+
+
+//We can also define a new function 
+	function generateAddress() {
+		console.log(faker.address.streetAddress());
+		console.log(faker.address.city());
+		console.log(faker.address.state());
+	}
+
